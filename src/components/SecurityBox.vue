5a1823562d63cda5ddba676c433749f5f08b9e93
tomohikoseven <tomohikoseven0905@gmail.com>
2021-08-24 08:02:51 +0900

    second commit.

---
+++ src/components/SecurityBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" :value="innerText" />
+</template>
+
+<script lang="ts">
+// import { defineComponent, reactive, ref } from "vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    // const state = reactive<{ messageOne: string }>({
+    //   messageOne: "Hello",
+    // });
+    const innerText = ref<string>("Helo.");
+
+    return {
+      innerText,
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,20 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
     <Sample />
+    <security-box />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Sample from "@/components/Sample.vue";
+import SecurityBox from "@/components/SecurityBox.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     Sample,
+    SecurityBox,
   },
 });
 </script>
