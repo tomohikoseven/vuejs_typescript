ddd3867945442fc3b3dd73ab1b86237c72c8c4b2
tomohikoseven <tomohikoseven0905@gmail.com>
2021-08-24 07:42:59 +0900

    first commit

---
+++ src/components/Sample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ state.messageOne }}</h2>
+    <h2>{{ messageTwo }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive<{ messageOne: string }>({
+      messageOne: "Hello",
+    });
+    const messageTwo = ref<string>("こんにちは");
+
+    return {
+      state,
+      messageTwo,
+    };
+  },
+});
+</script>
